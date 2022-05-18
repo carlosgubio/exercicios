62a234e070b0f9cda035c016660059c54316423a
carlos gubio santana moreira <carlosgubio@gmail.com>
2022-05-17 22:31:12 -0300

    Exercício

---
+++ 3 notas de 10 alunos/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _3_notas_de_10_alunos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nota = new int[10];
+            int i = 0;
+
+        }
+
+            
+    }
+}
---
+++ Array de 10 posições em ordem inversa/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Array_de_10_posições_em_ordem_inversa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int inversa = num();
+            Console.WriteLine($"A ordem inversa é: ");
+            
+        }
+
+        static int num()
+        {
+            int[] num = new int[10];
+            int contador = 0;
+            int inversa = 0;
+
+            for (int i = 0; i < num.Length; i++)
+            {
+                contador++;
+                Console.WriteLine($"Insira o {contador} º número");
+                num[i] = Convert.ToInt32(Console.ReadLine());
+                if (inversa < num[i])
+                {
+                    inversa = num[i];
+                }
+            }
+            return inversa;
+
+        }
+    }
+}
+
+
