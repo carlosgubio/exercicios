f9dce881b887ab1b15c3e7537ca4ecb2b2688bce
carlos gubio <carlosgubio@gmail.com>
2022-05-18 18:38:53 -0300

    Exercícios

--- 3 notas de 10 alunos/Program.cs
+++ 3 notas de 10 alunos/Program.cs
@@ -6,10 +6,30 @@ namespace _3_notas_de_10_alunos
     {
         static void Main(string[] args)
         {
-            int[] nota = new int[10];
-            int i = 0;
+            int[] aluno = new int[10];
+            int contador = 0;
+
+            for (int i = 0; i < aluno.Length; i++)
+            {
+                contador++;
+                Console.WriteLine($"Insira a nota do {contador}º aluno");
+                aluno[i] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+        static int nota()
+        {
+            int[] notas = new int[10];
+            int contador = 0;
+            
+            for (int i = 0; i < notas.Length; i++)
+            {
+                contador++;
+                Console.WriteLine($"Insira a nota do {contador}º aluno");
+                aluno1[i] = Convert.ToInt32(Console.ReadLine());
+            }
 
         }
+        
 
             
     }
--- Array de 10 posições em ordem inversa/Program.cs
+++ Array de 10 posições em ordem inversa/Program.cs
@@ -7,10 +7,7 @@ namespace Array_de_10_posições_em_ordem_inversa
         static void Main(string[] args)
         {
             int inversa = num();
-            Console.WriteLine($"A ordem inversa é: ");
-            
         }
-
         static int num()
         {
             int[] num = new int[10];
@@ -22,13 +19,13 @@ namespace Array_de_10_posições_em_ordem_inversa
                 contador++;
                 Console.WriteLine($"Insira o {contador} º número");
                 num[i] = Convert.ToInt32(Console.ReadLine());
-                if (inversa < num[i])
-                {
-                    inversa = num[i];
-                }
+            }
+
+            for (int num2 = 10; num2 > 0; num2--)
+            {
+                Console.WriteLine(num2);
             }
             return inversa;
-
         }
     }
 }
